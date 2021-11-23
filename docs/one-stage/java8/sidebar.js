f5657c02a89016cee035a5c7a50a10fb89b3020c
卢文龙 <1251601077@qq.com>
2021-11-23 13:16:22 +0800

    一阶段完成

---
+++ docs/one-stage/java8/sidebar.js
@@ -0,0 +1,18 @@
+module.exports = [
+	{
+		title:'JDK8新特性',
+		collapsable: false,
+		children:[
+			'java8/notes/lambda',
+			'java8/notes/function-interface',
+			'java8/notes/默认方法',
+			'java8/notes/stream',
+			'java8/notes/Optional',
+			'java8/notes/LocalDateTime',
+			'java8/notes/Nashorn',
+			'java8/notes/JUC-线程池',
+			'java8/notes/JUC-FutureTask',
+		]
+	},
+	
+]
--- docs/one-stage/threadsafe/sidebar.js
+++ docs/one-stage/threadsafe/sidebar.js
@@ -3,23 +3,26 @@ module.exports = [
 		title:'基础',
 		collapsable: false,
 		children:[
-			'java/notes/线程安全',
-			
+			'threadsafe/notes/a01.线程安全',
+			'threadsafe/notes/a02.ThreadLocal',
+			'threadsafe/notes/a04.synchronized',
+			'threadsafe/notes/a06.ReentrantLock',
 		]
 	},
 	{
 		title:'进阶',
 		collapsable: false,
 		children:[
-			
+			'threadsafe/notes/b00.condition',
+			'threadsafe/notes/b02.semaphore',
 		]
 	},
 	{
 		title:'高级',
 		collapsable: false,
 		children:[
-			
+			'threadsafe/notes/c00.atomic',
+			'threadsafe/notes/c02.CAS',
 		]
-	}
-	
+	},
 ]
