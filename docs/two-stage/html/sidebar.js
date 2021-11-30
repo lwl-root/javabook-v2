f0941bc4fe97f5269172977c41306f8d125bd0aa
卢文龙 <1251601077@qq.com>
2021-11-30 10:59:25 +0800

    前端基础

---
+++ docs/two-stage/html/sidebar.js
@@ -0,0 +1,24 @@
+module.exports = [
+	{
+		title:'HTML',
+		collapsable: false,
+		children:[
+			'html/notes/01.基本概念和结构',
+			'html/notes/02.单标签元素与双标签元素',
+			'html/notes/03.常用元素',
+			'html/notes/04.表单元素',
+			'html/notes/05.元素的分类',
+		]
+	},
+	{
+		title:'CSS',
+		collapsable: false,
+		children:[
+			'html/notes/01.CSS选择器',
+			'html/notes/02.Flex布局（上）',
+			'html/notes/03.Flex布局（下）',
+			'html/notes/04.CSS3新发现',
+		]
+	},
+    
+]
