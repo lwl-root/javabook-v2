f674afd4880d7aa51eac8fb62f1b3475273820c1
root <1251601077@qq.com>
2022-01-18 14:45:09 +0800

    新增日常积累模块

--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -1,25 +1,25 @@
 module.exports = {
-	//对多模块的管控
-	'/utilse/guava': require('../utilse/guava/sidebar'),
-	'/utilse/spring-utils': require('../utilse/spring-utils/sidebar'),
-	'/utilse/else': require('../utilse/else/sidebar'),
-	'/utilse/gist': require('../utilse/gist/sidebar'),
-	// '/utilse/markdown': require('../utilse/markdown/sidebar'),
-	'/utilse/docker': require('../utilse/docker/sidebar'),
-	
-	'/one-stage/javathread': require('../one-stage/javathread/sidebar'),
-	'/one-stage/java8': require('../one-stage/java8/sidebar'),
-	'/one-stage/java': require('../one-stage/java/sidebar'),
-	'/one-stage/myjdbc': require('../one-stage/myjdbc/sidebar'),
-	'/one-stage/threadsafe': require('../one-stage/threadsafe/sidebar'),
-	'/one-stage/mysql': require('../one-stage/mysql/sidebar'),
-	
-	'/two-stage/html': require('../two-stage/html/sidebar'),
-	'/two-stage/servlet': require('../two-stage/servlet/sidebar'),
-	'/two-stage/junit': require('../two-stage/junit/sidebar'),
-	'/two-stage/mybatis': require('../two-stage/mybatis/sidebar'),
+    //对多模块的管控
+    '/utilse/guava': require('../utilse/guava/sidebar'),
+    '/utilse/spring-utils': require('../utilse/spring-utils/sidebar'),
+    '/utilse/else': require('../utilse/else/sidebar'),
+    '/utilse/gist': require('../utilse/gist/sidebar'),
+    // '/utilse/markdown': require('../utilse/markdown/sidebar'),
+    '/utilse/docker': require('../utilse/docker/sidebar'),
 
-	'/pascal/basics': require('../pascal/basics/sidebar'),
+    '/one-stage/javathread': require('../one-stage/javathread/sidebar'),
+    '/one-stage/java8': require('../one-stage/java8/sidebar'),
+    '/one-stage/java': require('../one-stage/java/sidebar'),
+    '/one-stage/myjdbc': require('../one-stage/myjdbc/sidebar'),
+    '/one-stage/threadsafe': require('../one-stage/threadsafe/sidebar'),
+    '/one-stage/mysql': require('../one-stage/mysql/sidebar'),
 
-	'/demo/list': require('../demo/list/sidebar'),
+    '/two-stage/html': require('../two-stage/html/sidebar'),
+    '/two-stage/servlet': require('../two-stage/servlet/sidebar'),
+    '/two-stage/junit': require('../two-stage/junit/sidebar'),
+    '/two-stage/mybatis': require('../two-stage/mybatis/sidebar'),
+
+    '/pascal/basics': require('../pascal/basics/sidebar'),
+
+    '/demo/list': require('../demo/list/sidebar'),
 }
