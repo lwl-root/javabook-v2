7d2375b0cc8d2349139fc3731912f999042243ae
root <1251601077@qq.com>
2022-03-30 16:09:39 +0800

    添加AJAX请求模板

---
+++ docs/.vuepress/.temp/pages/utilse/gist/notes/axios.html.js
@@ -0,0 +1,51 @@
+export const data = {
+  "key": "v-0013797b",
+  "path": "/utilse/gist/notes/axios.html",
+  "title": "Axios 发起AJAX请求模板",
+  "lang": "zh-CN",
+  "frontmatter": {},
+  "excerpt": "",
+  "headers": [
+    {
+      "level": 2,
+      "title": "1、html页面引入Axios（使用CDN）",
+      "slug": "_1、html页面引入axios-使用cdn",
+      "children": []
+    },
+    {
+      "level": 2,
+      "title": "2、发起AJAX GET 请求",
+      "slug": "_2、发起ajax-get-请求",
+      "children": []
+    },
+    {
+      "level": 2,
+      "title": "3、发起AJAX POST 请求",
+      "slug": "_3、发起ajax-post-请求",
+      "children": []
+    },
+    {
+      "level": 2,
+      "title": "4、发起AJAX POST 请求",
+      "slug": "_4、发起ajax-post-请求",
+      "children": []
+    }
+  ],
+  "git": {
+    "updatedTime": null
+  },
+  "filePathRelative": "utilse/gist/notes/axios.md"
+}
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
